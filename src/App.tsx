8aee1965519d192ab705c6fbcbe795d3d6ae3a5d
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2024-04-24 20:41:01 -0600

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+  AuthPage,
+  ErrorComponent,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import routerBindings, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { Layout } from "./components/layout";
+import "./App.css";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { ForgotPassword } from "./pages/forgotPassword";
+import { authProvider } from "./authProvider";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            routerProvider={routerBindings}
+            authProvider={authProvider}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "6Pwv7v-89Qq3Q-KB404D",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
