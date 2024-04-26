503d783e7a7d3b5e40b7dacf6eb18eb19ba32c93
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2024-04-25 18:23:27 -0600

    SAFE-14 use Geist font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -71,7 +73,11 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ["Geist", ...fontFamily.sans],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
 };
+export default config;
