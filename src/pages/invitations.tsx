9ade38339f22f735b03cc3e3b5ea1ee05deb4645
Rodrigo Castrejon <47370853+rcastrejon@users.noreply.github.com>
2024-04-28 21:17:26 -0600

    SAFE-17 wip: implement dialog for add invitation

--- src/pages/invitations.tsx
+++ src/pages/invitations.tsx
@@ -3,6 +3,16 @@ import { InfinityTable, TablePaginationFooter } from "@/common/table";
 import { useClipboard } from "@/common/use-clipboard";
 import { Button } from "@/components/ui/button";
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
   TableBody,
   TableCell,
   TableHead,
@@ -11,6 +21,7 @@ import {
 } from "@/components/ui/table";
 import type { InvitationPublic } from "@/lib/types/invitation";
 import { useDelete } from "@refinedev/core";
+import { useModalForm } from "@refinedev/react-hook-form";
 import { useTable } from "@refinedev/react-table";
 import { type ColumnDef, flexRender } from "@tanstack/react-table";
 import { CirclePlus } from "lucide-react";
@@ -88,15 +99,50 @@ export function Invitations() {
     columns,
   });
 
+  const {
+    modal: { visible, show, close },
+    saveButtonProps,
+  } = useModalForm({
+    refineCoreProps: {
+      action: "create",
+    },
+  });
+
   return (
     <>
       <div className="flex items-end justify-between">
         <h3 className="font-semibold text-2xl leading-none tracking-tight">
           Invitations
         </h3>
-        <Button className="font-normal" size="sm">
-          <CirclePlus className="mr-2 h-4 w-4" /> Add Invitation
-        </Button>
+        <Dialog
+          open={visible}
+          onOpenChange={() => (visible ? close() : show())}
+        >
+          <DialogTrigger asChild>
+            <Button className="font-normal" size="sm">
+              <CirclePlus className="mr-2 h-4 w-4" /> Add Invitation
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add an invitation</DialogTitle>
+              <DialogDescription>
+                Are you sure you want to add a new invitation? Anyone with the
+                invitation code will be able to register an account.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button type="button" {...saveButtonProps}>
+                Accept
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="flow-root">
         <InfinityTable>
